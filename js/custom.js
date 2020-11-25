e160cc395a2ab13920f57ec1d8e81f2350ce6ee5
YassiSam <yasasisamarakoon@gmail.com>
2020-11-26 01:26:56 +0530

    projects

--- js/custom.js
+++ js/custom.js
@@ -45,7 +45,7 @@
     $('#service').parallax("100%", 0.2);
     $('#experience').parallax("100%", 0.3);
     $('#education').parallax("100%", 0.1);
-    $('#quotes').parallax("100%", 0.3);
+    $('#projects').parallax("100%", 0.3);
     $('#contact').parallax("100%", 0.1);
     $('footer').parallax("100%", 0.2);
 
